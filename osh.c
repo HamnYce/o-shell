f0222ab2fa2e66aeb5274320f697144b13e69a8d
HamnYce <hamadboa@gmail.com>
2023-02-02 15:53:25 +0300

    implement readline

--- osh.c
+++ osh.c
@@ -11,22 +11,39 @@
 void osh_loop() {
 }
 
-void osh_init() {
-  osh_loop();
+char *osh_readline(void) {
+  int buff_size = BUF_S;
+  int position = 0;
+  char *line = malloc(sizeof(char) * buff_size);
+  int c;
+
+  while (true) {
+    c = getchar();
+    if (c == '\n' || c == EOF) break;
+
+    line[position++] = c;
+
+    if (position >= buff_size) {
+      buff_size += BUF_S;
+      line = realloc(line, sizeof(char) * buff_size);
+    }
+  }
+  line[position] = '\0';
+
+  return line;
 }
 
-void osh_readline() {
+void osh_launch(int argc, char* argv[]) {
+  char *line = osh_readline();
 
+  printf("%s\n", line);
+}
+
+void osh_split_line() {
 }
 
 int main(int argc, char* argv[]) {
-  //osh_init();
+  osh_launch(argc, argv);
 
-  int size = read(STDIN, str, 10);
-  str[size] = '\0';
-
-  getchar();
-
-  printf("input string: %s", str);
   return 0;
 }
