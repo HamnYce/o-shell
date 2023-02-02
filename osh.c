394cc282f2ebd1dcce48f787e5b633c0bb190d1b
HamnYce <hamadboa@gmail.com>
2023-02-02 16:47:48 +0300

    implement osh_split_line

--- osh.c
+++ osh.c
@@ -4,8 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "builtins.c"
+#include <string.h>
 
 #define BUF_S 1024
+#define TOKENS_NUM 64;
+#define TOKENS_DELIM " \t\r\n\a"
 #define STDIN 0
 
 void osh_loop() {
@@ -17,6 +20,11 @@ char *osh_readline(void) {
   char *line = malloc(sizeof(char) * buff_size);
   int c;
 
+  if (line == NULL) {
+    printf("Allocation error: cannot allocate space for line");
+    exit(EXIT_FAILURE);
+  }
+
   while (true) {
     c = getchar();
     if (c == '\n' || c == EOF) break;
@@ -26,20 +34,65 @@ char *osh_readline(void) {
     if (position >= buff_size) {
       buff_size += BUF_S;
       line = realloc(line, sizeof(char) * buff_size);
+
+      if (line == NULL) {
+        printf("Allocation error: cannot allocate space for line");
+        exit(EXIT_FAILURE);
+      }
     }
   }
+
   line[position] = '\0';
 
   return line;
 }
 
+char **osh_split_line(char *line) {
+  int buff_size = TOKENS_NUM;
+  char **tokens = malloc(sizeof(char*) * buff_size);
+  char *token;
+  int position = 0;
+ 
+  if (tokens == NULL) {
+    printf("Allocation error: cannot allocate space for tokens");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, TOKENS_DELIM);
+  while (true) {
+    if (token == NULL) break;
+
+    tokens[position] = token;
+    position++;
+
+    if (position >= buff_size) {
+      buff_size += TOKENS_NUM;
+      tokens = realloc(tokens, sizeof(char*) * buff_size);
+
+      if (tokens == NULL) {
+        printf("Allocation error: cannot allocate space for tokens");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, TOKENS_DELIM);
+  }
+
+  tokens[position] = NULL;
+
+  return tokens;
+}
+
 void osh_launch(int argc, char* argv[]) {
   char *line = osh_readline();
 
-  printf("%s\n", line);
-}
+  char **tokens = osh_split_line(line);
 
-void osh_split_line() {
+  printf("tokens:\n");
+  while (*tokens != NULL) {
+    printf("%s\n", *tokens);
+    tokens++;
+  }
 }
 
 int main(int argc, char* argv[]) {
